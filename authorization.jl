d321e70fd342b1e82e1af4a93eba47fa009c6354
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-07-29 12:03:54 -0400

    auth flow nearing complete

--- authorization.jl
+++ authorization.jl
@@ -4,7 +4,10 @@ using Revise
 using HTTP
 using JSON
 using Base64
+using Parameters
+
 include("credentials.jl")
+import Base64.base64encode
 
 ##
 # I can't believe I lost all my work
@@ -17,3 +20,30 @@ body = "grant_type=client_credentials"
 HTTP.post(URL, HEADER, body)
 
 ##
+@with_kw struct SpotifyCredentials
+    client_id::String
+    client_secret::String
+    encoded_credentials::String = ""
+    access_token::String = "" 
+end
+##
+SC = SpotifyCredentials(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+
+##
+function base64encode(sc::SpotifyCredentials)
+    encoded_creds = base64encode(sc.client_id * ":" * sc.client_secret)
+    return SpotifyCredentials(client_id=sc.client_id, client_secret = sc.client_secret, encoded_credentials = encoded_creds)
+end
+
+##
+SC = base64encode(SC)
+
+##
+function GetAuthorizationToken(sc::SpotifyCredentials)
+    header = ["Authorization" => "Basic $(sc.encoded_credentials)","Content-Type" => "application/x-www-form-urlencoded"]
+    body = "grant_type=client_credentials"
+    return HTTP.post(URL, header, body)
+end
+
+##
+GetAuthorizationToken(SC)
