8ee85b63bc59495e0242bff7fa34aeb4d50d840e
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-07-29 11:46:30 -0400

    Recovered

--- authorization.jl
+++ authorization.jl
@@ -4,7 +4,16 @@ using Revise
 using HTTP
 using JSON
 using Base64
+include("credentials.jl")
 
 ##
 # I can't believe I lost all my work
+ENCODED_CREDS = base64encode(CLIENT_ID*":"*CLIENT_SECRET)
 
+##
+URL = "https://accounts.spotify.com/api/token"
+HEADER = ["Authorization" => "Basic $ENCODED_CREDS", "Content-Type" => "application/x-www-form-urlencoded"]
+body = "grant_type=client_credentials"
+HTTP.post(URL, HEADER, body)
+
+##
