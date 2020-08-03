64bdaa31db6f56335bc035806cbf5930f6a635d7
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-08-03 19:35:07 -0400

    add track_get_audio_features

--- src/tracks.jl
+++ src/tracks.jl
@@ -15,4 +15,18 @@ function tracks_get_audio_analysis(track_id)
     return spotify_request("audio-analysis/$track_id")
 end
 
-##
+
+
+
+## https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/
+@doc """
+# Get Audio Features for a Track
+**Summary**: Get audio feature information for a single track identified by it's unique Spotify ID
+
+`track_id` _Required_: The Spotify ID for the track.
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/tracks/get-audio-features/)
+""" ->
+function tracks_get_audio_features(track_id)
+    return spotify_request("audio-features/$track_id")
+end
