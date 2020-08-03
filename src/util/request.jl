add08ed3c1b41499ac6fd187270cae160e0a3272
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-08-03 14:28:56 -0400

    updated header

--- src/util/request.jl
+++ src/util/request.jl
@@ -3,6 +3,9 @@
 function spotify_request(url_ext::String, method::String="GET")
     #println("Request URL:       https://api.spotify.com/v1/$url_ext")
     #HTTP.payload(HTTP.request($method,"https://api.spotify.com/v1/$url_ext", header), String) |> JSON.parse
-    return HTTP.request($method,"https://api.spotify.com/v1/$url_ext", header)
+    return HTTP.request(method,"https://api.spotify.com/v1/$url_ext", header_maker())
 
 end
+
+##
+spotify_request("browse/categories")
