bc5056831b74c5cd85ab0959633770ebf1e2028d
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-08-14 12:22:30 -0400

    finish docs for PUT and DEL

--- src/library.jl
+++ src/library.jl
@@ -96,7 +96,14 @@ end
 #### DELETE ####
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/remove-albums-user/
+@doc """
+# Remove Albums for Current User
+**Summary**: Remove one or more albums for the current user's 'Your Music' library.\n 
 
+`album_ids` _Required_: A comma-separated list of the Spotify IDs. Maximum 50.\n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/remove-albums-user/)
+""" ->
 function library_remove_albums(album_ids)
     return spotify_request("me/albums?ids=$album_ids", method="DELETE")
 end
@@ -104,14 +111,28 @@ end
 
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/remove-shows-user/
+@doc """
+# Remove Shows for Current User
+**Summary**: Remove one or more shows for the current user's library.\n 
 
+`show_ids` _Required_: A comma-separated list of the Spotify IDs. Maximum 50.\n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/remove-shows-user/)
+""" ->
 function library_remove_shows(show_ids)
     return spotify_request("me/shows?ids=$show_ids", method="DELETE")
 end
 
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/remove-tracks-user/
+@doc """
+# Remove Tracks for Current User
+**Summary**: Remove one or more tracks for the current user's 'Your Music' library.\n 
 
+`track_ids` _Required_: A comma-separated list of the Spotify IDs. Maximum 50.\n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/remove-tracks-user/)
+""" ->
 function library_remove_tracks(track_ids)
     return spotify_request("me/tracks?ids=$track_ids", method="DELETE")
 end
@@ -119,20 +140,41 @@ end
 #### PUT #####
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/save-albums-user/
+@doc """
+# Save Albums for Current User
+** Summary**: Save one or more albums to the current user's 'Your Music' library.\n 
 
+`album_ids` _Required_: A comma-separated list of Spotify IDs. Maximum 50. \n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/save-albums-user/)
+""" ->
 function library_save_album(album_ids)
     return spotify_request("me/albums?ids=$album_ids", method="PUT")
 end
 
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/save-shows-user/
+@doc """
+# Save Shows for Current User
+** Summary**: Save one or more shows to the current user's library.\n 
 
+`shows_ids` _Required_: A comma-separated list of Spotify IDs. Maximum 50. \n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/save-shows-user/)
+""" ->
 function library_save_show(shows_ids)
     return spotify_request("me/shows?ids=$show_ids", method="PUT")
 end
 
 
 ## https://developer.spotify.com/documentation/web-api/reference/library/save-tracks-user/
+@doc """
+# Save Tracks for Current User
+** Summary**: Save one or more tracks to the current user's 'Your Music' library.\n 
 
+`track_ids` _Required_: A comma-separated list of Spotify IDs. Maximum 50. \n
+
+[Reference](https://developer.spotify.com/documentation/web-api/reference/library/save-tracks-user/)
+""" ->
 function library_save_track(track_ids)
     return spotify_request("me/tracks?ids=$track_ids", method="PUT")
