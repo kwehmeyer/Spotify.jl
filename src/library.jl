03ff100c3d07b411f126eb32419f0dc036f1b51b
Kahlil Wehmeyer <kahlilwehmeyer@gmai.com>
2020-08-14 11:40:29 -0400

    finish get methods for library

--- src/library.jl
+++ src/library.jl
@@ -1,2 +1,14 @@
 # library.jl
 ## https://developer.spotify.com/documentation/web-api/reference/library/
+
+## https://developer.spotify.com/documentation/web-api/reference/library/get-users-saved-tracks/
+
+function library_get_saved_tracks(limit=20, offset=0, market="US")
+    return spotify_request("me/tracks?market=$market&offset=$offset&limit=$limit")
+end
+
+## https://developer.spotify.com/documentation/web-api/reference/library/get-users-saved-shows/
+
+function library_get_saved_shows(limit=20, offset=0)
+    return spotify_request("me/shows?limit=$limit&offset=$offset")
+end
