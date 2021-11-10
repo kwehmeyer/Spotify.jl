612e64e6a2a90b3050a744ddb942e459b5a25df1
Kahlil Wehmeyer <kahlilwehmeyer@pm.me>
2021-11-10 11:09:14 -0500

    documentation automation

--- docs/make.jl
+++ docs/make.jl
@@ -3,3 +3,7 @@ using Documenter, Spotify
 
 makedocs(sitename="Spotify.jl Documentation",
 format = Documenter.HTML(prettyurls=false))
+
+deploydocs(
+    repo = "github.com/kwehmeyer/Spotify.jl.git",
+)
