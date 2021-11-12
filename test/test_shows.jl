cce5b804f911de67ecc78ba2358d519da96c21eb
Vikas Negi <vikas.negi10@gmail.com>
2021-11-13 00:23:43 +0100

    Add shows to export structure

--- test/test_shows.jl
+++ test/test_shows.jl
@@ -1,5 +1,7 @@
 # Run tests on functions in src/by_reference_doc/shows.jl
 
+using Spotify.Shows
+
 @testset verbose = true "GET-request endpoints for shows" begin
 
     # Input argument from composite types defined in src/types.jl
@@ -11,17 +13,17 @@
 
     @testset "For market = $(market_id)" for market_id in markets
 
-        @test ~isempty(Spotify.show_get_single(show_id, market = "$market_id")[1])
+        @test ~isempty(show_get_single(show_id, market = "$market_id")[1])
 
-        @test ~isempty(Spotify.show_get_multiple("$show_id, 4rOoJ6Egrf8K2IrywzwOMk", market = "$market_id")[1])
+        @test ~isempty(show_get_multiple("$show_id, 4rOoJ6Egrf8K2IrywzwOMk", market = "$market_id")[1])
 
-        @test ~isempty(Spotify.show_get_episodes(show_id, market = "$market_id")[1])
+        @test ~isempty(show_get_episodes(show_id, market = "$market_id")[1])
 
     end
 
-    @test ~isempty(Spotify.show_get_saved()[1])
+    @test ~isempty(show_get_saved()[1])
 
     # Currently shows 403 error, implemented in shows.jl
-    @test_skip ~isempty(Spotify.show_get_contains("2MAi0BvDc6GTFvKFPXnkCL")[1])
+    @test_skip ~isempty(show_get_contains("2MAi0BvDc6GTFvKFPXnkCL")[1])
 
 end
